1bed8d2fdc03907eff5cd5efabba05b097b8a2d3
JacekKorta <jacek.korta@gmail.com>
2022-09-11 12:46:11 +0200

    Add cpe searcher

---
+++ cpe/cpe.go
@@ -0,0 +1,73 @@
+package cpe
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+type Client struct {
+	http   *http.Client
+	apiKey string
+}
+
+func (c *Client) FetchAll(query string) (*CpeResponse, error) {
+	endpoint := fmt.Sprintf("https://services.nvd.nist.gov/rest/json/cpes/1.0/?keyword=%s", url.QueryEscape(query))
+
+	resp, err := c.http.Get(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(string(body))
+	}
+
+	res := &CpeResponse{}
+	return res, json.Unmarshal(body, res)
+}
+
+type CpeResponse struct {
+	ResultsPerPage int    `json:"resultsPerPage"`
+	StartIndex     int    `json:"startIndex"`
+	TotalResults   int    `json:"totalResults"`
+	Result         Result `json:"result"`
+}
+
+type Result struct {
+	DataType      string `json:"dataType"`
+	FeedVersion   string `json:"feedVersion"`
+	CpeCount      int    `json:"cpeCount"`
+	FeedTimestamp string `json:"feedTimestamp"`
+	Cpes          []Cpe  `json:"cpes"`
+}
+
+type Cpe struct {
+	Deprecated       bool          `json:"deprecated"`
+	Cpe23URI         string        `json:"cpe23Uri"`
+	LastModifiedDate string        `json:"lastModifiedDate"`
+	Titles           []Title       `json:"titles"`
+	Refs             []interface{} `json:"refs"`
+	DeprecatedBy     []interface{} `json:"deprecatedBy"`
+	Vulnerabilities  []interface{} `json:"vulnerabilities"`
+}
+
+type Title struct {
+	Title string `json:"title"`
+	Lang  string `json:"lang"`
+}
+
+// ===========================
+
+func NewClient(httpClient *http.Client, apiKey string) *Client {
+	return &Client{httpClient, apiKey}
+}
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"go-nist-api/cpe"
 )
 
 var tmpl = template.Must(template.ParseFiles("index.html"))
@@ -15,6 +19,26 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
+func searchHandler(cpeApi *cpe.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		params := u.Query()
+		keyword := params.Get("q")
+
+		response, err := cpeApi.FetchAll(keyword)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Printf("%+v", response)
+	}
+}
 
 func main() {
 	err := godotenv.Load()
@@ -27,15 +51,22 @@ func main() {
 		port = "9000"
 	}
 
+	apiKey := os.Getenv("NEWS_API_KEY")
+	// if apiKey == "" {
+	// 	log.Fatal("Env: apiKey must be set")
+	// }
+
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	cpeApi := cpe.NewClient(myClient, apiKey)
+
 	fs := http.FileServer(http.Dir("assets"))
 
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/search", searchHandler(cpeApi))
 	mux.HandleFunc("/", indexHandler)
 
 	http.ListenAndServe(":"+port, mux)
 
-
 }
-
-
