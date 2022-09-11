9a0a6a954a8302725d426bf052dcbfbab34b54d6
JacekKorta <jacek.korta@gmail.com>
2022-09-11 10:58:13 +0200

    Add search bar

--- main.go
+++ main.go
@@ -1,1 +1,41 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var tmpl = template.Must(template.ParseFiles("index.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl.Execute(w, nil)
+}
+
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("error during loading the .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9000"
+	}
+
+	fs := http.FileServer(http.Dir("assets"))
+
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.HandleFunc("/", indexHandler)
+
+	http.ListenAndServe(":"+port, mux)
+
+
+}
+
 
